f5a3a9660d3322314d0debe7f7f63ac6c96d832b
Ruslan2205 <54228532+Ruslan2205@users.noreply.github.com>
2025-12-20 04:59:31 +0300

    251219

---
+++ database/migrations/2025_12_20_010956_create_units_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('units', function (Blueprint $table) {
+            $table->comment('единицы измерений');
+            $table->id();
+            $table->string('notation', length: 255)->unique()->comment('обозначение');
+            $table->string('name', length: 255)->comment('имя');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('units');
+    }
+};
